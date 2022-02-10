26adbde6fca1e3a3e19ad2729ed2b93a32e52d0b
mohitwasnik132 <mohit.wasnik132@gmail.com>
2022-02-10 20:05:47 +0530

    separate Security Group file added for clarity of code

---
+++ DockrInstall/SG.tf
@@ -0,0 +1,34 @@
+resource "aws_security_group" "allpubsg" {
+  name = "Public-SecurityGroup"
+  description = "allows all traffic from SSH HTTP HTTPS"
+  # vpc_id =  
+
+  ingress {
+    cidr_blocks = [ "0.0.0.0/0" ]
+    description = "ALL IN SSH"
+    from_port = 22
+    protocol = "tcp"
+    to_port = 22
+  } 
+
+  ingress {
+    cidr_blocks = [ "0.0.0.0/0" ]
+    description = "ALL IN HTTP"
+    from_port = 80
+    protocol = "tcp"
+    to_port = 80
+  } 
+
+    egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ALL_SG"
+  }
+}
+
+
