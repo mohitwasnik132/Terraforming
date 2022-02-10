76548de4ad11c75fa16d4593e276f3502ff27fd7
mohitwasnik132 <mohit.wasnik132@gmail.com>
2022-02-10 20:08:32 +0530

    fmt done on SG

--- DockrInstall/SG.tf
+++ DockrInstall/SG.tf
@@ -1,25 +1,25 @@
 resource "aws_security_group" "allpubsg" {
-  name = "Public-SecurityGroup"
+  name        = "Public-SecurityGroup"
   description = "allows all traffic from SSH HTTP HTTPS"
   # vpc_id =  
 
   ingress {
-    cidr_blocks = [ "0.0.0.0/0" ]
+    cidr_blocks = ["0.0.0.0/0"]
     description = "ALL IN SSH"
-    from_port = 22
-    protocol = "tcp"
-    to_port = 22
-  } 
+    from_port   = 22
+    protocol    = "tcp"
+    to_port     = 22
+  }
 
   ingress {
-    cidr_blocks = [ "0.0.0.0/0" ]
+    cidr_blocks = ["0.0.0.0/0"]
     description = "ALL IN HTTP"
-    from_port = 80
-    protocol = "tcp"
-    to_port = 80
-  } 
+    from_port   = 80
+    protocol    = "tcp"
+    to_port     = 80
+  }
 
-    egress {
+  egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
