17a6dd527fd693c7099d5ecd41d1abba3e6af34a
mohitwasnik132 <mohit.wasnik132@gmail.com>
2022-02-11 11:08:46 +0530

    http_ssh ssh access added

--- DockrInstall/SG.tf
+++ DockrInstall/SG.tf
@@ -1,12 +1,12 @@
 module "http-sg" {
   source = "terraform-aws-modules/security-group/aws"
 
-  name        = "http_sg"
-  description = "Security group for HTTP/HTTPS/ICMP access"
+  name        = "http_ssh"
+  description = "Security group for HTTP/HTTPS/SSH access"
   vpc_id      = data.aws_vpc.default.id
-
   ingress_cidr_blocks = ["0.0.0.0/0"]
-  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
+  ingress_rules       = ["http-80-tcp", "https-443-tcp","ssh-tcp"]
+  egress_rules        = ["all-all"]
   #Follow 
   #https://github.com/terraform-aws-modules/terraform-aws-security-group/blob/master/rules.tf   variable "auto_groups" for better understanding 
 
