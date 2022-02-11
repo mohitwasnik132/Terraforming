b465a36afdfc41940ee4c14701cc83dacfe1d771
mohitwasnik132 <mohit.wasnik132@gmail.com>
2022-02-11 11:07:50 +0530

    added ssh to sg, name changwd http_ssg

--- DockrInstall/main.tf
+++ DockrInstall/main.tf
@@ -1,8 +1,14 @@
+resource "aws_key_pair" "mykey" {
+  key_name   = "mykey"
+  public_key = file(var.pubkey)
+}
+
+
 resource "aws_instance" "DockerReady" {
   ami           = "ami-0851b76e8b1bce90b"
   instance_type = "t2.micro"
   key_name      = aws_key_pair.mykey.key_name
-  # subnet_id              = 
+  # subnet_id     =  
   vpc_security_group_ids = [local.sgpub]
   user_data              = file("dockerinstall.sh")
 
@@ -19,39 +25,32 @@ resource "aws_instance" "DockerReady" {
 
 }
 
-resource "aws_key_pair" "mykey" {
-  key_name   = "mykey"
-  public_key = file(var.pubkey)
-}
-
-
-
-
-
-
-
-
-
-
-
-
 resource "aws_instance" "instancetrial" {
-  ami           = "ami-0851b76e8b1bce90b"
-  instance_type = "t2.micro"
-  key_name      = aws_key_pair.mykey.key_name
-  user_data     = file("trial.sh")
+  ami                    = "ami-0851b76e8b1bce90b"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [local.sgpub]
+  key_name               = aws_key_pair.mykey.key_name
+  user_data              = file("trial.sh")
+
+  connection {
+    user        = var.username
+    private_key = file(var.prikey)
+    host        = self.public_ip
+  }
 
 
+  provisioner "remote-exec" {
+    inline = [
+      "touch works.txt",
+      "echo I am tryna be sensible >> works.txt",
+    ]
+  }
   tags = {
     Name    = "1Ready"
     PROJECT = "teradoc"
   }
 
-  connection {
-    user        = var.username
-    private_key = file(var.privkey)
-    host        = self.public_ip
-  }
+
 }
 
 
