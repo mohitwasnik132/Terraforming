8ae30e0b4b1f5711658f2a28f9be206e65a8665f
mohitwasnik132 <69758819+mohitwasnik132@users.noreply.github.com>
2022-03-10 19:53:09 +0530

    Adding data source file
    Amazon Linux2 and Ubuntu 20.x are sourced here

---
+++ Remote and Local Modules/ec2/data_source.tf
@@ -0,0 +1,37 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = ["099720109477"]
+}
+
+
+#=============AMAZON-LATEST=========================#
+
+data "aws_ami" "amazon" {
+  owners      = ["amazon"]
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-ebs"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+}
+
+
